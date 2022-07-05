bcc1189a3669e0b25d1cc519cba2b42bd1171af7
Adam-Teng <656081024@qq.com>
2022-07-04 22:25:11 -0700

    [add] a initial py test

--- ics-toy/multimod/main.c
+++ ics-toy/multimod/main.c
@@ -1,11 +1,13 @@
+#include <assert.h>
+#include <inttypes.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <inttypes.h>
+#include <stdlib.h>
 
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 
 void test(uint64_t a, uint64_t b, uint64_t m) {
-  #define U64 "%" PRIu64
+#define U64 "%" PRIu64
   printf(U64 " * " U64 " mod " U64 " = " U64 "\n", a, b, m, multimod(a, b, m));
 }
 
@@ -13,4 +15,10 @@ int main() {
   test(123, 456, 789);
   test(123, 456, -1ULL);
   test(-2ULL, -2ULL, -1ULL); // should be 1
+  char buf[100];
+  FILE *fp = popen("python3 multimod.py", "r");
+  assert(fp);
+  fscanf(fp, "%s", buf);
+  printf("popen() returns: %s\n", buf);
+  pclose(fp);
 }
