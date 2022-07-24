5daa4048d83a7de499d9023ae9a76b3254d64e86
Adam-Teng <656081024@qq.com>
2022-07-24 03:57:32 -0700

    add test, update README

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 A basic linux pstree command writing in pure C.
 
+## asm
+
+modify some C code and function with C inline assembly language 
+
 ## two little problems
 
 ### multimod
--- ics-toy/asm/main.c
+++ ics-toy/asm/main.c
@@ -1,6 +1,8 @@
 #include "asm.h"
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
   asm_jmp_buf buf;
@@ -8,7 +10,27 @@ int main() {
   if (r == 0) {
     assert(asm_add(1234, 5678) == 6912);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
-    // TODO: add more tests here.
+    assert(asm_add(0, 999) == 999);
+    assert(asm_add(0xffffffffffffffff, 2) == 1);
+
+    assert(asm_popcnt(0xffffffffffffffffull) == 64);
+    assert(asm_popcnt(1) == 1);
+    assert(asm_popcnt(2) == 1);
+    assert(asm_popcnt(3) == 2);
+    assert(asm_popcnt(4) == 1);
+
+    assert(asm_memcpy(NULL, NULL, 0) == NULL);
+    char s1[20] = "Peoples'";
+    char s2[20] = " Republic";
+    char s3[20] = "";
+    char s4[20] = "Purple";
+    asm_memcpy(s3, s1, 20);
+    assert(strcmp(s3, "Peoples'") == 0);
+    asm_memcpy(s3 + 8, s2, 20);
+    assert(strcmp(s3, "Peoples' Republic") == 0);
+    asm_memcpy(s3, s4, 3);
+    assert(strcmp(s3, "Pur") == 0);
+
     asm_longjmp(buf, 123);
   } else {
     assert(r == 123);
