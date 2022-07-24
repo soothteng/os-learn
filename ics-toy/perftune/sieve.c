0c781667637b6a725292380cc498fe8f5051e9a9
Adam-Teng <656081024@qq.com>
2022-07-24 02:03:44 -0700

    finish ics perftune

--- ics-toy/perftune/sieve.c
+++ ics-toy/perftune/sieve.c
@@ -12,14 +12,21 @@ int *sieve(int n) {
   assert(n + 1 < N);
   for (int i = 0; i <= n; i++)
     is_prime[i] = true;
+  is_prime[2] = false;
 
-  for (int i = 2; i <= n; i++) {
-    for (int j = i + i; j <= n; j += i) {
-      is_prime[j] = false;
+  for (int i = 3; i <= n; i+=2) {
+    if (is_prime[i]) {
+      int product = i * i, step = 2 * i;
+      if (product > n)
+        break;
+      for (int j = product; j <= n; j += step) {
+        is_prime[j] = false;
+      }
     }
   }
 
   int *p = primes;
+  *p++ = 2;
   for (int i = 2; i <= n; i++)
     if (is_prime[i]) {
       *p++ = i;
